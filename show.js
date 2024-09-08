172408e88a6e1606f984db35f89851471d4cc3b8
Hamza-nagori <126978565+Hamza-nagori@users.noreply.github.com>
2024-09-08 18:40:39 +0530

    Add files via upload

---
+++ show.js
@@ -0,0 +1,12 @@
+function show(){
+    const sidebar = document.querySelector('.sidebar')
+    const side = document.querySelector('.hide')
+    sidebar.style.display = 'flex'
+    // side.style.display = 'none'
+}
+function hide(){
+    const sidebar = document.querySelector('.sidebar')
+    const side = document.querySelector('.hide')
+    sidebar.style.display = 'none'
+    // side.style.display = 'flex'
+}
